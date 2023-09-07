1bab3ee210ac142fa4b0ecf5b92dbccc02efea50
Ashikur Rahman <ashikur.jebu111@gmail.com>
2023-09-07 21:10:20 +0600

    Till the making of category routes

---
+++ e-com-backend/app.js
@@ -0,0 +1,25 @@
+require("express-async-errors");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const morgan = require("morgan");
+const error = require("./middlewares/error");
+const userRouter = require("./routers/userRouter");
+const categoryRouter=require('./routers/categoryRouter')
+
+//! ---- Middlewares ----- 
+app.use(express.json());
+app.use(cors());
+
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
+
+//! ----- Routers -----
+app.use("/api/user", userRouter);
+app.use("/api/category", categoryRouter);
+
+//! This function is for handling async await error.
+app.use(error);
+
+module.exports = app;
---
+++ e-com-backend/routers/userRouter.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const { signUp, signIn } = require("../controllers/userController");
+
+router.route("/signup").post(signUp);
+
+router.route("/signin").post(signIn);
+
+module.exports = router;
